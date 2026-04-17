b602763136d0996541daeacbd653c5431f3bb28a
Anjanaph21 <anjanahegdee21@gmail.com>
2026-04-17 16:16:24 +0530

    feat: add arithmetic, relational, logical, and assignment operator examples

---
+++ JS_ESSENTIALS/operators.js
@@ -0,0 +1,12 @@
+//Arithmetic operators +, -, *, /, %, **
+console.log(Math.floor(10/3)) // why we use Math.floor() ? -> to get the integer part only. 
+console.log(10 % 3) // modulo operator -> gives the remainder
+console.log(2 ** 3) // exponentiation operator -> gives the power
+
+
+
+
+//Relational operators <, >, <=, >=, ==, !=, ===, !==
+//Logical operators &&, ||, !
+//Assignment operators =, +=, -=, *=, /=, %=, **=
+//Ternary operator ? : 

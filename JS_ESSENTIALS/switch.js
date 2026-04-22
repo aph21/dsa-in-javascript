632affb81c9482929fc69867db9799ebb42ca553
Anjanaph21 <anjanahegdee21@gmail.com>
2026-04-22 16:35:23 +0530

    feat: add documentation and syntax example for switch statements

---
+++ JS_ESSENTIALS/switch.js
@@ -0,0 +1,15 @@
+//switch case is also one of the conditional statement only
+// it is used to make decisions
+//how it is different from if-else?
+// if-else is used to make decisions based on conditions
+// switch case is used to make decisions based on values
+// syntax:
+// switch(value){
+//     case value1: statement1;
+//     break;
+//     case value2: statement2;
+//     break;
+//     case value3: statement3;
+//     break;
+//     default: statement4;
+// }

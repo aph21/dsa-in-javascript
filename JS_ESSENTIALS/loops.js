fd0c82a44986abd6c0fdc857e4e03f7e57134520
Anjanaph21 <anjanahegdee21@gmail.com>
2026-04-23 20:53:47 +0530

    feat: add loop exercises covering natural numbers, factorials, factors, and range sums

--- JS_ESSENTIALS/loops.js
+++ JS_ESSENTIALS/loops.js
@@ -42,3 +42,59 @@ let prompt = require("prompt-sync")();
 
 
 //4. factorial of a number
+//factorial of number n is written as n!
+// n! means multiply all positive integers from 1 upto n.
+// if n= 5, then 5! = 5 * 4 * 3 * 2 * 1 = 120
+// 0! is a special case, 0! = 1
+//n! = n × (n-1) × (n-2) × ... × 1
+
+
+// let n = Number(prompt("Enter a number: "));
+// if(n < 0){
+//     console.log("Factorial of a negative number is not defined");
+// }else if(n === 0 || n === 1){
+//     console.log(1);
+    
+// }else{
+//     let fact = 1;
+//     for(let i = 2; i<= n; i++){
+//         fact = fact * i;
+//     }
+//     console.log(fact);
+// }
+
+
+// 5. Print all factors of a Number
+// a factor is a number n is any number that divides n completely (i.e leaves remainder 0)
+// example: factors of 12 are 1, 2, 3, 4, 6, 12
+// *** KEY RULE: A factor of n is always between 1 and n(inclusive)
+
+// let n = Number(prompt("Enter a number: "));
+// if(n < 0 || n === 0){
+//     console.log("Factors of negative number and 0 are not defined");  
+// }else{
+//     for(let i = 1; i<= n; i++){
+//         if(n % i === 0){
+//             process.stdout.write(i + " ");
+//         }
+//     }
+// }
+
+//6. Sum of even and odd numbers in range
+let start = Number(prompt("Enter the starting number: "));
+let end = Number(prompt("Enter the ending number: "));
+let sumEven = 0;
+let sumOdd = 0;
+
+if(start > end) {
+    [start, end] = [end, start];
+}
+for(let i = start; i <= end; i++){
+    if(i % 2 === 0){
+        sumEven = sumEven + i;
+    }else{
+        sumOdd = sumOdd + i;
+    }
+}
+console.log("Sum of even numbers: ", sumEven);
+console.log("Sum of odd numbers: ", sumOdd);

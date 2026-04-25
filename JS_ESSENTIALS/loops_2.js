41aca8d9dd2b665ef50e7b191e604b45b73dc94d
Anjanaph21 <anjanahegdee21@gmail.com>
2026-04-25 12:02:34 +0530

    feat: add examples and exercises for while loop implementation

---
+++ JS_ESSENTIALS/loops_2.js
@@ -0,0 +1,35 @@
+// WHILE LOOP: 
+// syntax: while(condition) { // block of code}
+//definition: a type of entry control loop. while loop will run repeatedly until the condition is true.
+//example:
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+//difference between for and while loop:
+// for loop is used when we know the number of iterations
+// while loop is used when we dont know the number of iterations
+
+
+//example:extract digits of a number
+// let n = Number(prompt("Enter a number: "));
+// while(n > 0){
+//     let digit = n % 10;
+//     console.log(digit);
+//     n = Math.floor(n / 10);
+// }
+
+//1. Reverse number:
+//n = 589 should be 985
+// let prompt = require('prompt-sync')();
+// let n = Number(prompt("Enter a number: "));
+// let rev = 0;
+// while(n > 0){
+//     let dig = n % 10;
+//     rev = (rev * 10) + dig;
+//     n = Math.floor(n / 10);
+// }
+// console.log(rev);
+
+//2. sum of digits in a number

8e6bdcfb8c01892022d1cbb34ea3f7b226e2341f
Anjanaph21 <anjanahegdee21@gmail.com>
2026-04-26 12:46:49 +0530

    feat: add while loop examples and automorphic number checker script

--- JS_ESSENTIALS/loops_2.js
+++ JS_ESSENTIALS/loops_2.js
@@ -1,3 +1,4 @@
+let prompt = require('prompt-sync')();
 // WHILE LOOP: 
 // syntax: while(condition) { // block of code}
 //definition: a type of entry control loop. while loop will run repeatedly until the condition is true.
@@ -33,3 +34,31 @@
 // console.log(rev);
 
 //2. sum of digits in a number
+// let n = Number(prompt("Enter a number: "));
+// let sum = 0;
+// while(n > 0){
+//     let dig = n % 10;
+//     sum = sum + dig;
+//     n = Math.floor(n / 10);
+// }
+// console.log(sum);
+
+
+// 3. Automorphic number:
+// a number is called automorphic if its square ends with the number itself
+// example: 5^2 = 25, 6^2 = 36, 25^2 = 625
+
+let n = Number(prompt("Enter a number:"));
+let sq  = n * n;
+let temp = n;
+let count = 0;
+while(temp > 0){
+    count++;
+    temp = Math.floor(temp / 10);
+}
+let pow = Math.pow(10, count);
+if(sq % pow == n){
+    console.log("Automorphic");
+}else{
+    console.log("Not Automorphic");
+}

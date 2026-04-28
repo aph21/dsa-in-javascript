f13440d776d29734734b0addf13df7baca833c61
Anjanaph21 <anjanahegdee21@gmail.com>
2026-04-28 15:56:56 +0530

    feat: add JavaScript conditional and loop logic exercises and update prompt tracker

--- JS_ESSENTIALS/loops_2.js
+++ JS_ESSENTIALS/loops_2.js
@@ -48,17 +48,50 @@ let prompt = require('prompt-sync')();
 // a number is called automorphic if its square ends with the number itself
 // example: 5^2 = 25, 6^2 = 36, 25^2 = 625
 
-let n = Number(prompt("Enter a number:"));
-let sq  = n * n;
-let temp = n;
-let count = 0;
-while(temp > 0){
-    count++;
-    temp = Math.floor(temp / 10);
-}
-let pow = Math.pow(10, count);
-if(sq % pow === n){
-    console.log("Automorphic");
-}else{
-    console.log("Not Automorphic");
-}
+// let n = Number(prompt("Enter a number:"));
+// let sq  = n * n;
+// let temp = n;
+// let count = 0;
+// while(temp > 0){
+//     count++;
+//     temp = Math.floor(temp / 10);
+// }
+// let pow = Math.pow(10, count);
+// if(sq % pow === n){
+//     console.log("Automorphic");
+// }else{
+//     console.log("Not Automorphic");
+// }
+
+
+// 1. Identifythe ISBN number
+// what is ISBN number: it is an unique identifier assigned to every published books
+//ISBN stands for International Standard Book Number
+// it is a 13 digit number or 10 digit number
+// to verify it we use two separate validation rule for 13-digit and 10-digit
+
+// let n = prompt("Enter a number: ");
+// let copy = n, count = 0;
+
+// while(n >0){
+//     count++;
+//     n = Math.floor(n/10); //this while block counts the number of digits in a number
+// }
+// if(count != 10) console.log("Invalid ISBN");
+// else{
+//     let ans = 0;
+//     while(copy > 0){
+//         let dig = copy % 10;
+//         ans = ans + (dig * count);
+//         count--;
+//         copy = Math.floor(copy / 10);
+//     }
+//     if(ans % 11 === 0){
+//         console.log("Valid ISBN");
+//     }else{
+//         console.log("Invalid ISBN"); 
+//     }
+// }
+
+
+//2. Reverse a Number

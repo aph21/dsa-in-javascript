94c63abe7f2414429fd8436f26c9a4eccfea8b4d
Anjanaph21 <anjanahegdee21@gmail.com>
2026-04-28 16:59:32 +0530

    feat: add examples and exercises for do-while loops in JavaScript

---
+++ JS_ESSENTIALS/loops_3.js
@@ -0,0 +1,37 @@
+// Do while loop -> it is an exit control loop. It will run the block of code at least once and then check the condition. If the condition is true, it will run the block of code again. Else it will terminate.
+// Syntax: do { // block of code } while(condition);
+// Example:
+// let i = 100;
+// do {
+//     console.log(i);
+//     i--;
+// } while(i >= 1);
+
+//1. print sum of first N natural numbers
+let prompt = require('prompt-sync')();
+// let n = Number(prompt("Enter a number: "));
+// let sum = 0;
+// let i = 1;
+// do {
+//     sum = sum + i;
+//     i++;
+// } while(i <= n);
+// console.log(sum);
+
+//2. Guess the number:
+// let computer = Math.floor((Math.random() * 100) + 1);
+// let user;
+// do{
+// user = Number(prompt("Enter a number between 1 and 100: "));
+// if(user > computer){
+//     console.log("Too Large!");
+// }else if(user < computer){
+//     console.log("Too Small!");
+// }else if(user === computer){
+//     console.log("Congratulations!👏🎉🎊 You guessed the number correctly!");
+//     break;
+// }else{
+//     console.log("Invalid Input!!!");
+//     console.log("\n");
+// }
+// }while(user != computer);
